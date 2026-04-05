1b86bf6bfef2cc987cf74b44347ddd369fc870c1
SohaibAli <sohaib.ali@potomac.com>
2026-04-05 09:58:29 -0400

    Cleanup repository: remove old docs, unused files, organize utilities

--- create_vms.py
+++
@@ -1,4 +0,0 @@
-import base64, os  
-base = r'C:\Users\SohaibAli\Documents\PotomacDeveloper\ViewModels'  
-files = {}  
-files['ResearcherViewModel.cs'] = base64.b64decode(open('r_b64.txt').read()) 
